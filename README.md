d4171196fc6371818c354fab44bfbf75ce379c72
Tabean19 <Tabean19@gmail.com>
2019-09-03 02:01:16 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple 'To do' app created with React and Axios -- Using JSONPlaceholder as a backend. 
---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This page was built to help facilitate communication between NOC personnel.
+          It was created with React in the front-end, Axios to handle HTTP requests and a PHP backend.
+      </p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
